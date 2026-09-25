05e440055df10295df56fe3cbd55c6b642bdaac8
Kristopher Cargile <k@algorythmic.com>
2026-09-24 22:26:47 -0600

    Update lint-staged configuration to exclude markdown files from formatting

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,5 +3,5 @@
 /** @type {import("lint-staged").Configuration} */
 export default {
 	"./**/*.{ts,cts,mts,js,cjs,mjs,json,sh,yaml,yml}": "pnpm exec biome lint",
-	"./**/*": "pnpm exec biome format --write",
+	"./**/!(*.md)": "pnpm exec biome format --write",
 };
